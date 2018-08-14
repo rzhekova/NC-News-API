879e616971ed7d97eae8f2ee3015c5cbd0b78cb7
rzhekova <rzhekova@gmail.com>
2018-08-14 15:41:43 +0100

    added comments to usersrouter

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,5 +5,8 @@ usersRouter.get("/", (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 usersRouter.get("/:username", getUserByUsername);
+// edit get request above to say users/:username
+
+// add get request for all users
 
 module.exports = usersRouter;
